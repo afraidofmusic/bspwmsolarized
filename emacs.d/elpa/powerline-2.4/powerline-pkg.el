3192931bfd9738032b73084b69fa5c685d6d9b1b
AfraidOfMusic <canaksu@protonmail.com>
2018-05-14 07:11:34 +0300

    Cleanup

--- emacs.d/elpa/powerline-2.4/powerline-pkg.el
+++
@@ -1,8 +0,0 @@
-(define-package "powerline" "2.4" "Rewrite of Powerline"
-  '((cl-lib "0.2"))
-  :keywords
-  '("mode-line")
-  :url "http://github.com/milkypostman/powerline/")
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- zprofile
+++
@@ -1,5 +0,0 @@
-# Aliases
-alias ls='ls --color'
-alias neofetch='neofetch --ascii_colors 2 7 --colors 2 7 7 2'
-## Path
-export PATH=$PATH:${HOME}/.local/bin
